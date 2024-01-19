b0ae61afe19eee582d832ba5313e4868f1ac1e81
jjuanda19 <jg8115y@gre.ac.uk>
2024-01-19 00:30:43 +0000

    Trying google mas API

---
+++ RemainderApplication/app/src/main/java/com/example/remainderapplication/MapsActivity.kt
@@ -0,0 +1,118 @@
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.example.remainderapplication.R
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+abstract class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    // New variables for Current Place picker
+    private val TAG = "MapsActivity"
+    private lateinit var lstPlaces: ListView
+    private lateinit var mPlacesClient: PlacesClient
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private var mLastKnownLocation: Location? = null
+
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private val mDefaultLocation = LatLng(-33.8523341, 151.2106085)
+    private val DEFAULT_ZOOM = 15
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    private var mLocationPermissionGranted = false
+
+    // Used for selecting the Current Place.
+    private val M_MAX_ENTRIES = 5
+    private lateinit var mLikelyPlaceNames: Array<String>
+    private lateinit var mLikelyPlaceAddresses: Array<String>
+    private lateinit var mLikelyPlaceAttributions: Array<String>
+    private lateinit var mLikelyPlaceLatLngs: Array<LatLng>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        // Set up the action toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Set up the views
+        lstPlaces = findViewById(R.id.listPlaces)
+
+        // Initialize the Places client
+        val apiKey = getString(R.string.action_geolocate)
+        Places.initialize(applicationContext, apiKey)
+        mPlacesClient = Places.createClient(this)
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.manu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_geolocate -> {
+                // COMMENTED OUT UNTIL WE DEFINE THE METHOD
+                // Present the current place picker
+                // pickCurrentPlace()
+                return true
+            }
+
+            else -> {
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+
+
+}
